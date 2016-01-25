45b7854d3d2d61fee7c3b9f1769fbcebd8de063e
Constantin <kostiantynkalinin@gmail.com>
2016-01-25 13:23:36 +0000

    challenge 5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,10 @@
 class Oystercard
-attr_reader :balance
-def initialize
+ attr_reader :balance
+ def initialize
   @balance = 0
+ def top_up amount
+  @balance += amount
 
 end
-end
+ end
+  end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require 'Oystercard'
 describe Oystercard do
 it {is_expected.to respond_to(:balance)}
 
+it 'can be topped up' do
+  oyster = Oystercard.new
+
+  oyster.top_up(100)
+  expect(subject.balance).to eq 100
+end
 end
