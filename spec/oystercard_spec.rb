c8bdb3f9f0f7da6a5a5b8b8b5e3ebab9b8bdd3cc
yiorgoss <makridakisyiorgos@gmail.com>
2016-01-25 18:48:58 +0000

    top up method and test complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:card){described_class.new}
 	it "new card balance == 0" do
-		expect(subject.balance).to eq 0
+		expect(card.balance).to eq 0
 	end
+  context "Changing Balance" do
+    it "Topping up Balance" do
+      expect{card.top_up(10)}.to change{ card.balance}.by 10
+    end
+    it "" do
+
+    end
+
+  end
 end
