7bbdae4671855138b7e21f15e00e06632e6c13e2
ZeshanRasul <zeshanrasul90@gmail.com>
2016-01-25 15:59:17 +0000

    Added top_up and deduct methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,19 @@
 class Oystercard
- attr_reader :balance
- def initialize
-  @balance = 0
- def top_up amount
-  @balance += amount
 
-end
- end
+  MAXIMUM_BALANCE = 90
+
+ attr_reader :balance
+
+  def initialize
+    @balance = 0
   end
+
+  def top_up amount
+    fail "Balance has exceeded limit of #{MAXIMUM_BALANCE}" if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct amount
+    @balance -= amount
+  end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,33 @@
 require 'Oystercard'
+
 describe Oystercard do
-it {is_expected.to respond_to(:balance)}
 
-it 'can be topped up' do
-  oyster = Oystercard.new
+  it { is_expected. to respond_to{:balance}}
 
-  oyster.top_up(100)
-  expect(oyster.balance).to eq 100
-end
+  describe 'top up' do
+
+
+    it {is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can be topped up' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'top up cannot allow balance to exceed £90' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{subject.top_up(1)}.to raise_error "Balance has exceeded limit of #{maximum_balance}"
+    end
+
+  end
+
+  describe 'deduct method' do
+    it 'deduct' do
+      subject.top_up 50
+      expect{ subject.deduct 50 }.to change{ subject.balance }.by -50
+    end
+  end
+
+
+
 end
