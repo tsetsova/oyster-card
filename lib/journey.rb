d5ebadcf7d816d12b29e6028c144cc0d3f537a2a
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-27 16:01:01 +0000

    Exit_station variable added to Jouney class.

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,9 +1,13 @@
 class Journey
 
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
 
   def initialize entry_station
     @entry_station = entry_station
+  end
+
+  def end_journey station
+    @exit_station = station
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,9 +5,17 @@ describe Journey do
 subject(:journey) { described_class.new oystercard.entry_station }
 let(:oystercard) {double :oystercard}
 
-  it 'start a journey' do
-    allow(oystercard).to receive(:entry_station).and_return 'aldgate'
+before do
+  allow(oystercard).to receive(:entry_station).and_return 'aldgate'
+  allow(oystercard).to receive(:exit_station).and_return 'brockley'
+end
+
+  it 'starts a journey' do
     expect(journey.entry_station).to eq 'aldgate'
+  end
+
+  it 'ends a journey' do
+    expect(journey.end_journey(oystercard.exit_station)).to eq 'brockley'
   end
 
 end
