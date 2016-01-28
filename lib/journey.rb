1a9be488b975089bdfa04973aaf073772a78d4fa
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-28 12:13:03 +0000

    Fare calculater tested and added in Journey class.

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,11 +1,12 @@
 class Journey
   PENALTY_FARE = 6
   MINIMUM_CHARGE = 1
-  
+
   attr_reader :entry_station, :exit_station
 
   def initialize(station)
     @entry_station = station
+    @exit_station = nil
   end
 
   def end_journey station
@@ -13,7 +14,8 @@ class Journey
   end
 
   def calculate_fare
-    PENALTY_FARE
+    return PENALTY_FARE if exit_station.nil?
+    MINIMUM_CHARGE
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -8,6 +8,7 @@ let(:exit_station) {double :station}
 
 before do
   allow(entry_station).to receive(:zone).and_return 1
+  allow(exit_station).to receive(:zone).and_return 3
   allow(entry_station).to receive(:name).and_return 'aldgate'
   allow(exit_station).to receive(:name).and_return 'brockley'
 end
@@ -29,8 +30,23 @@ end
   end
 
   describe '#calculate_fare' do
-    it 'deducts a penalty charge if the user does not touch in or out' do
-      expect(subject.calculate_fare).to eq described_class::PENALTY_FARE
+    context 'when the user does not touch in' do
+      let(:new_journey) {described_class.new}
+      it 'returns a penalty charge' do
+        expect(subject.calculate_fare).to eq described_class::PENALTY_FARE
+      end
+    end
+    context 'when the user does not touch out' do
+      it 'returns a penalty charge' do
+        expect(journey.calculate_fare).to eq described_class::PENALTY_FARE
+      end
+    end
+
+    context 'when the user completes a journey' do
+      it 'calculates the correct fare' do
+        journey.end_journey(exit_station)
+        expect(journey.calculate_fare).to eq described_class::MINIMUM_CHARGE
+      end
     end
   end
 
