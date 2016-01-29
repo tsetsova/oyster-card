c24f1ac2ee138efa681e53254c3c761d11c679d0
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-29 14:30:35 +0000

    Journey_log intialize and start_journey methods tested and produced.

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,14 +1,14 @@
-require 'journey'
+require_relative 'journey'
 
 class Journey_log
   attr_reader :log_history, :entry_station
 
-  def initialize(station, journey_class: Journey)
+  def initialize(journey_class: Journey)
     @log_history = []
-    @entry_station = station
   end
 
-  def start_journey
+  def start_journey station
     @journey = Journey.new
+    @entry_station = station
   end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,27 +1,33 @@
 require 'journey_log'
 
 describe Journey_log do
-  subject(:journey_log) {described_class.new station journey_class: journey_class}
+  subject(:journey_log) {described_class.new(journey_class: journey_class)}
   let(:station) {double :station}
+  let(:journey_class) {double(:journey_class, new: journey)}
   let(:journey) {double :journey}
-  before do
-    allow
-  end
+
   context '#initialize' do
     it 'has an empty history' do
       expect(journey_log.log_history).to be_empty
     end
 
-    it 'has an entry_station' do
-      expect(journey_log.entry_station).to eq station
-    end
   end
 
   context '#start_journey' do
-    it 'instantiates a new journey' do
-      expect(journey).to receive(:new)
-      journey_log.start_journey
+    xit 'instantiates a new journey' do
+      expect(journey_class).to receive(:new)
+      journey_log.start_journey station
     end
+
+    context 'when receiving a new station' do
+      before do
+        journey_log.start_journey station
+      end
+      it 'has an entry_station' do
+        expect(journey_log.entry_station).to eq station
+      end
+    end
+
   end
 
   context '#current_journey' do
